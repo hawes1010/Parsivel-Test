36664c29db0b09d53d54ad275740e3610ad834bd
Hawes Collier <37671636+hawes1010@users.noreply.github.com>
2020-02-06 11:33:03 -0500

    Update Parsivel-Test.ino

--- Parsivel-Test.ino
+++ Parsivel-Test.ino
@@ -8,17 +8,60 @@
 
 // set this to the hardware serial port you wish to use
 #define HWSERIAL Serial1
-double intensity;
-double rain_size;
-double rain_attribute_0;
-double rain_attribute_1;
-double rain_attribute_2;
-double rain_attribute_3;
-int data_number = 0;
- int array_size;
+String Parsivel_ID;
+String intensity;
+String rain_size;
+String rain_attribute_0;
+String rain_attribute_1;
+String rain_attribute_2;
+String rain_attribute_3;
+int command_num = 0;
+int array_size;
+//#include <array>
+//#include <iostream>
+
 void setup() {
 	Serial.begin(9600);
-	HWSERIAL.begin(38400);
+	HWSERIAL.begin(19200);
+}
+
+
+
+void clearArray(char* a){
+  int len = sizeof(a) / sizeof(char);
+  int length = len;
+  int i;
+
+  for (i = 0; i < length; i++)
+    a[i] = NULL;
+}
+
+void loop() {
+    char* arr;
+    int data_pointer = 0;
+      while (HWSERIAL.available()) {
+      char c =  HWSERIAL.read();
+      
+      if (c == 0xA) { //new line feed
+        String datastring = convertToString(arr, array_size);
+        command_num = 0;
+        data_pointer = 0;
+        clearArray(arr);
+        continue;
+      }
+      if (c == 0x3B){ //semicolon that separates out the values from parsivel
+       data_pointer = 0;
+       String datastring = convertToString(arr, array_size);
+       parseMessage(datastring, command_num);
+       command_num++;
+       clearArray(arr);
+        continue;
+      }
+     arr[data_pointer++] = c;
+     int array_size = sizeof(arr) / sizeof(char); 
+   
+      }
+  
 }
 
 String convertToString(char* a, int size) 
@@ -31,45 +74,52 @@ String convertToString(char* a, int size)
     return s; 
 } 
 
-void clearArray(char* a){
-  
+
+void parseMessage(String a, int comm){
+   switch(comm){
+    case 0: 
+    Parsivel_ID = a;
+    break;
+    
+    case 1:
+    intensity = a;
+    break;
+
+    case 2:
+    rain_size = a;
+    break;
+
+    case 3:
+    rain_attribute_0 = a;
+    break;
+
+    case 4:
+    rain_attribute_1 = a;
+    break;
+
+    case 5:
+    rain_attribute_2 = a;
+    break;
+
+    default:
+    //save the data: SD card?  
+    break;
+   }
 }
 
-void loop() {
-    char arr[70];
-    int data_pointer = 0;
-      while (Serial.available()) {
-      char c =  Serial.read();
-
-      //Serial.print(c, HEX); Serial.print("#"); Serial.println(c);
-      
-      if (c == 0xA) { //new line feed
-        String datastring = convertToString(arr, array_size);
-        data_number = 0;
-        continue;
-      }
-      if (c == 0x3B){ //semicolon that separates out the values from parsivel
-       
-        continue;
-      }
-     arr[data_pointer++] = c;
-     int array_size = sizeof(arr) / sizeof(char); 
-   
-      }
-  /*
-	if (Serial.available() > 0) {
-		incomingByte = Serial.read();
-		Serial.print("USB received: ");
-		Serial.println(incomingByte, DEC);
-		HWSERIAL.print("USB received:");
-		HWSERIAL.println(incomingByte, DEC);
-	}
-	if (HWSERIAL.available() > 0) {
-		incomingByte = HWSERIAL.read();
-		Serial.print("UART received: ");
-		Serial.println(incomingByte, DEC);
-		HWSERIAL.print("UART received:");
-		HWSERIAL.println(incomingByte, DEC);
-	}
+/*
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    Serial.print("USB received: ");
+    Serial.println(incomingByte, DEC);
+    HWSERIAL.print("USB received:");
+    HWSERIAL.println(incomingByte, DEC);
+  }
+  if (HWSERIAL.available() > 0) {
+    incomingByte = HWSERIAL.read();
+    Serial.print("UART received: ");
+    Serial.println(incomingByte, DEC);
+    HWSERIAL.print("UART received:");
+    HWSERIAL.println(incomingByte, DEC);
+  }
  */
-}
